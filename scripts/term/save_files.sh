8b6551329fdee241981a030eaeb7d305edfc3fbd
Jonathan Almstedt <jonal155@student.liu.se>
2023-03-25 17:10:31 +0100

    commit 3

--- scripts/term/save_files.sh
+++ scripts/term/save_files.sh
@@ -28,7 +28,7 @@ echo "$new_number"
 cd /home/jonalm/desktopgit/Desktop/
 git add --all
 git commit -m "commit ${new_number}"
-git push -u -f origin master
+git push -u -f origin main
 
 if [ $? -eq 0 ]; then
   echo "{ \"number\": $new_number }" | jq . > "$json_file"
