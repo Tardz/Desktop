2b6edecaa4636268306dca350e64ca2f13143273
Tardz <jonathan.almstedt@live.se>
2023-03-29 15:41:37 +0200

    commit 13

--- scripts/term/gitpush.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-alacritty -e cd "/home/jonalm/docs/Labbar/"
-alacritty -e git add --all
-alacritty -e git commit -m "upload"
-alacritty -e git push -u -f origin master
--- scripts/term/gitpushlabb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-alacritty -e cd $HOME/jonalm/docs/Labbar git add --all git commit -m "upload" git push -u -f origin master
